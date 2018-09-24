3cb2f0decb348182bd0251721fa278ef8e274f24
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-24 00:30:46 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -56,10 +56,10 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season,  holiday|
-    puts "#{season.to_s.capitalize}:"
-    holiday.each do |holiday, supply|
-     puts "#{holiday.to_s.capitalize}: #{supply.join(', ')}"
-     end
+      puts "#{season.to_s.capitalize}:"
+    holiday.each do |event, supply|
+      puts " #{event.to_s.split("_").each{|item| item.capitalize!}.join(" ")}: #{supply.join(", ")}"
+    end
     end
 
 def all_holidays_with_bbq(holiday_hash)
