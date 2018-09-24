f5cf1585c3628d074b8586ad59d94b44654a229f
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-24 00:31:35 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,9 +59,9 @@ def all_supplies_in_holidays(holiday_hash)
       puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supply|
       puts " #{event.to_s.split("_").each{|item| item.capitalize!}.join(" ")}: #{supply.join(", ")}"
+      end
     end
-    end
-
+end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
