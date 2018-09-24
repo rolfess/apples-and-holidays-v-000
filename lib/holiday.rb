17a7cfe1f6c40838b30724f42c22d9b8012c8cfc
Susan Rolfes <rolfes57069@yahoo.com>
2018-09-24 00:32:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season,  holiday|
       puts "#{season.to_s.capitalize}:"
     holiday.each do |event, supply|
-      puts " #{event.to_s.split("_").each{|item| item.capitalize!}.join(" ")}: #{supply.join(", ")}"
+      puts "  #{event.to_s.split("_").each{|item| item.capitalize!}.join(" ")}: #{supply.join(", ")}"
       end
     end
 end
